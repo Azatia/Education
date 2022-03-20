21d2e94465547d93c7da43ccfae03b5623c32601
azatia <azat_too@mail.ru>
2022-03-20 14:53:33 +0600

    20 March

--- Lesson6/MethodOverloading.java
+++ Lesson6/MethodOverloading.java
@@ -2,9 +2,25 @@ package Lesson6;
 
 public class MethodOverloading {
 
-    void showInt (int i1) {
+    void show(int i1) {
 
         System.out.println(i1);
+    }
+
+    void show(boolean b1) {
+        System.out.println(b1);
+    }
+
+    void show(String s1) {
+
+        System.out.println(s1);
+        System.out.println("Azat is always sexy");
+
+    }
+
+    void show(String s, int a) {
+        System.out.println("String :" + s + "int :" + a);
+
 
     }
 
@@ -14,7 +30,12 @@ class MethodOverloadingTest{
     public static void main (String[] args) {
         MethodOverloading mO = new MethodOverloading();
         int a = 500;
-        mO.showInt(a);
+        mO.show(a);
+        boolean b = true;
+        mO.show(b);
+
+        String s = "Privet";
+        mO.show(s);
     }
 
 
